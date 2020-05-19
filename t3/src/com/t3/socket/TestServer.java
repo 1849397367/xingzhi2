493d31e85b1a6f1b2c5506de95a5ad4a8bf98e2a
zhujnan <247176894@qq.com>
2020-05-19 14:14:21 +0800

    添加监听获取socket对象的输入流
    Signed-off-by: zhujnan <247176894@qq.com>

--- t3/src/com/t3/socket/TestServer.java
+++ t3/src/com/t3/socket/TestServer.java
@@ -1,7 +1,10 @@
 package com.t3.socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TestServer {
 
@@ -10,6 +13,14 @@ public class TestServer {
 		ServerSocket serverSocket
 			= new ServerSocket(10087);
 		
+		Socket client = serverSocket.accept();
+		System.out.println(client
+				.getInetAddress()
+					.getHostAddress()+"client");
+		BufferedReader burfin
+			= new BufferedReader(
+					new InputStreamReader(
+							client.getInputStream()));
 	}
 
 }
