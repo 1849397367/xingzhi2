cfc30150cd7dc5952f0aaeaf5da97119e9f70592
zhujnan <247176894@qq.com>
2020-05-19 14:21:55 +0800

    添加了新的代码 Signed-off-by: zhujnan <247176894@qq.com>

--- t3/src/com/t3/socket/TestServer.java
+++ t3/src/com/t3/socket/TestServer.java
@@ -2,7 +2,10 @@ package com.t3.socket;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,11 +20,25 @@ public class TestServer {
 		System.out.println(client
 				.getInetAddress()
 					.getHostAddress()+"client");
+		
+		InputStream in = client.getInputStream();
+		//获取字节输入流
 		BufferedReader burfin
 			= new BufferedReader(
 					new InputStreamReader(
 							client.getInputStream()));
 		
+		PrintWriter pw
+			=new PrintWriter(client.getOutputStream(),true);
+		pw.print("success");
+		byte[] buf = new byte[1024];
+		int len = in.read(buf);
+		System.out.println(new String(buf,0,len));
+		
+		OutputStream out 
+			= client.getOutputStream();
+		out.write("hello客户端".getBytes());
+		client.close();
 		
 	}
 
