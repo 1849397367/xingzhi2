da0a59152aba35d0174d3e58383cfe2da791009a
zhujnan <247176894@qq.com>
2020-05-20 16:28:43 +0800

    jdbc查询功能实现
    Signed-off-by: zhujnan <247176894@qq.com>

--- jdbc01/src/main/java/com/jdbc01/demo/Jdbc01.java
+++ jdbc01/src/main/java/com/jdbc01/demo/Jdbc01.java
@@ -4,7 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
+
+import com.mysql.jdbc.Statement;
 
 public class Jdbc01 {
 	private static String url;
@@ -19,24 +25,51 @@ public class Jdbc01 {
 			props.load(fileInputStream);
 			//4.属性文件对象加载指定的文件对象
 			url = props.getProperty("url");
-			dbUser = props.getProperty("dbUser");
-			dbPassword = props.getProperty("dbPassword");
+			dbUser = props.getProperty("username");
+			dbPassword = props.getProperty("password");
 			System.out.println(url);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
 	}
-	
+	private static void getEmpData() throws SQLException {
+		Connection conn = null;//连接对象
+		Statement stmt = null;//语句对象
+		ResultSet rs = null;//结果集
+		String sql = "select * from person";
+		
+		conn = DriverManager
+				.getConnection(url,dbUser,dbPassword);
+		stmt = (Statement) conn.createStatement();
+		rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			int age = rs.getInt("age");
+			System.out.println(id+","+name+","+age);
+		}
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
 	public static void main(String[] args) {
 		//1.读取属性文件的值
 		try {
 			getParams("src/main/resources/pro.properties");
+			try {
+				getEmpData();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+	
 
 	
 }
