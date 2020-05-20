ce71c7ea777c87a7e722a5b800b510c03f7f8631
zhujnan <247176894@qq.com>
2020-05-20 16:18:37 +0800

    获取文件属性的值 Signed-off-by: zhujnan <247176894@qq.com>

--- jdbc01/src/main/java/com/jdbc01/demo/Jdbc01.java
+++ jdbc01/src/main/java/com/jdbc01/demo/Jdbc01.java
@@ -1,5 +1,42 @@
 package com.jdbc01.demo;
 
-public class Jdbc01 {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Jdbc01 {
+	private static String url;
+	private static String dbUser;
+	private static String dbPassword;
+	private static void getParams(String fileName) throws IOException {
+		Properties props = new Properties();//1.创建属性文件对象
+		File file = new File(fileName);//2.创建File对象
+		try {
+			FileInputStream fileInputStream
+				= new FileInputStream(file);//3.文件输入流对象
+			props.load(fileInputStream);
+			//4.属性文件对象加载指定的文件对象
+			url = props.getProperty("url");
+			dbUser = props.getProperty("dbUser");
+			dbPassword = props.getProperty("dbPassword");
+			System.out.println(url);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		//1.读取属性文件的值
+		try {
+			getParams("src/main/resources/pro.properties");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
 }
